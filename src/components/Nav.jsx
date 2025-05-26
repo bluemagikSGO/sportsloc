4120b5291965fa48cf7296a22821b42bb635f5b5
bluemagikSGO <stevengabriel2015@gmail.com>
2025-05-26 22:26:25 +0100

    26/05/2025

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -60,10 +60,10 @@ const Nav = () => {
             Find Player
           </a>
           <a href="#">Events</a>
-          <button className="w-4/5 border-2 border-black hover:border-blue-500 py-2  rounded-md">
+          <button className="w-4/5 border-2 border-black hover:border-blue-500 py-2  rounded-md focus:text-blue-500">
             Login
           </button>
-          <button className="w-4/5 border-2 border-black hover:border-blue-500 py-2 rounded-md">
+          <button className="w-4/5 border-2 border-black hover:border-blue-500 py-2 rounded-md focus:text-blue-500">
             Sign Up
           </button>
         </div>
